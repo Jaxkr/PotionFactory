3b235fc39cbc788d1c9a4d5d55901e8e5745b7c2
Jackson Roberts <jacksonroberts25@gmail.com>
2016-10-14 18:02:27 -0700

    new tooltips

--- js/game.js
+++ js/game.js
@@ -62,8 +62,7 @@ $(document).ready(function() {
   resume();
   showPanes();
   displayOrders();
-  $( document ).tooltip();
-
+  $('.tooltip').tooltipster();
   if (PLAYER_DATA['UNLOCKED_PANES'].indexOf('orders') > -1) {
     setTimeout(newOrder, getRandomInt(20 * 1000, 600 * 1000)); // 20 and 600 seconds
   }
